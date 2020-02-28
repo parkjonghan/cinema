37bd00f13896a61d06efe3599c6cce33159158c3
parkjoghan <bluebird78dev@gmail.com>
2020-02-29 00:05:16 +0900

    selector 를 이용한 좋아요 싫어요 구현

--- app/src/main/java/com/kptech/cinema/MainActivity.kt
+++ app/src/main/java/com/kptech/cinema/MainActivity.kt
@@ -8,10 +8,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    enum class SELECT_LIKE_TYPE  { NONE,LIKE, DISLIKE}
     private  var likeCnt :Int =50
     private  var disLikeCnt : Int  =50
-    private  var selectedLikeType : SELECT_LIKE_TYPE = SELECT_LIKE_TYPE.NONE
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,31 +20,24 @@ class MainActivity : AppCompatActivity() {
 
         btn_like.setOnClickListener {
 
-            if(selectedLikeType==SELECT_LIKE_TYPE.LIKE)
+            if(btn_like.isChecked)
                 return@setOnClickListener
-            selectedLikeType=SELECT_LIKE_TYPE.LIKE
             likeCnt++
             if(disLikeCnt>0)
                 disLikeCnt--
-            likeCntText.text = likeCnt.toString()
-            dislikeCntText.text = disLikeCnt.toString()
-            btn_dislike.setImageResource(R.drawable.ic_thumb_down)
-            btn_like.setImageResource(R.drawable.ic_thumb_up_selected)
+            btn_like.text = "  "+likeCnt.toString()
+            btn_dislike.text = "  "+disLikeCnt.toString()
         }
 
         btn_dislike.setOnClickListener {
 
-            if(selectedLikeType==SELECT_LIKE_TYPE.DISLIKE)
+            if(btn_dislike.isChecked)
                 return@setOnClickListener
-            selectedLikeType=SELECT_LIKE_TYPE.DISLIKE
             disLikeCnt++
             if(likeCnt>0)
                 likeCnt--
-            likeCntText.text = likeCnt.toString()
-            dislikeCntText.text = disLikeCnt.toString()
-            btn_like.setImageResource(R.drawable.ic_thumb_up)
-            btn_dislike.setImageResource(R.drawable.ic_thumb_down_selected)
-
+            btn_like.text = "  "+likeCnt.toString()
+            btn_dislike.text = "  "+disLikeCnt.toString()
         }
 
 
